0b7a877fd7f4256ae5b7e78f43462d5ec38fa39f
Mikita Kalashnikau <nikita.kalashnikau@aetsoft.net>
2023-04-26 03:07:39 +0200

    Add ID to display in template

--- src/pages/Area/index.tsx
+++ src/pages/Area/index.tsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom";
 
 const AreaPage = () => {
   const { id } = useParams();
-  return (<div>Area page!!</div>)
+  return (<div>Area page!! ${id}</div>)
 };
 
 export default AreaPage;
--- src/pages/Products/index.tsx
+++ src/pages/Products/index.tsx
@@ -2,7 +2,7 @@ import { useParams } from "react-router-dom";
 
 const ProductsPage = () => {
   const { id } = useParams();
-  return (<div>Products page!!</div>)
+  return (<div>Products page!! ${id}</div>)
 };
 
 export default ProductsPage;
